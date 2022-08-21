3467c433f62b027a4c273dbdea1622d5ff7e886b
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-08-20 21:21:27 -0400

    Create gameObjects array to pass all 5 background layers

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 15;
+let gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'images/layer-1.png';
@@ -27,7 +27,7 @@ class Layer {
         this.speed = gameSpeed * this.speedModifier;
     }
     update() {
-        this.x -= this.speed * this.speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
         if (this.x <= -this.width) {
             this.x = this.width + this.x2 - this.speed;
         }
@@ -40,12 +40,23 @@ class Layer {
     draw() {
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
-
     }
 }
 
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    });
     requestAnimationFrame(animate);
 };
 animate();
