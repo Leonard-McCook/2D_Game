6dacd074ce4aa49b0528a8d66b560c6240db4892
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-08-24 17:04:46 -0400

    Create animation movement pattern

--- script.js
+++ script.js
@@ -6,29 +6,32 @@ const CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = 'images/enemy1.png';
+let gameFrame = 0;
 
 class Enemy {
     constructor(){
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.speed = Math.random() * 4 - 2;
+        this.image = new Image();
+        this.image.src = 'images/enemy1.png';
+        // this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update() {
-        this.x+= this.speed;
-        this.y+= this.speed;
+        this.x+= Math.random() * 5 - 2.5;
+        this.y+= Math.random() * 5 - 2.5;
         //animate sprites
-        this.frame > 4 ? this.frame = 0 : this.frame++;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
     }
     draw() {
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, 
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, 
         this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 };
@@ -43,7 +46,8 @@ function animate() {
     enemiesArray.forEach(enemy => {
         enemy.update();
         enemy.draw();
-    })
+    });
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
