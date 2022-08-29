a519b23755d512a1d64cdfd57ad7e8f08eac4eb9
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-08-29 18:40:18 -0400

    Build explosion animation class

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-/**@type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 500;
-const CANVAS_HEIGHT = canvas.height = 700;
+canvas.width = 500;
+canvas.height = 700;
+const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
+
+class Explosion {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.spriteWidth  = 200;
+        this.spriteHeight = 179;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+        this.image = new Image();
+        this.image.src = './images/boom.png';
+        this.frame = 0;
+    }
+    update() {
+        this.frame++;
+    }
+    draw() {
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+
+window.addEventListener('click', function(e) {
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
+});
+
