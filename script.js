b399f3d6568f7a444ad3bc4503625c5ae3fa1b11
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-08-30 23:47:50 -0400

    Build function to animate explostion

--- script.js
+++ script.js
@@ -7,18 +7,22 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
         this.spriteWidth  = 200;
         this.spriteHeight = 179;
-        this.width = this.spriteWidth/2;
-        this.height = this.spriteHeight/2;
+        this.width = this.spriteWidth * 0.7;
+        this.height = this.spriteHeight * 0.7;
+        this.x = x - this.width / 2;
+        this.y = y - this.height / 2;
         this.image = new Image();
         this.image.src = './images/boom.png';
         this.frame = 0;
+        this.timer = 0;
     }
     update() {
-        this.frame++;
+        this.timer++;
+        if (this.timer % 10 === 0) {
+            this.frame++;
+        };
     }
     draw() {
         ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
@@ -31,3 +35,12 @@ window.addEventListener('click', function(e) {
     explosions.push(new Explosion(positionX, positionY));
 });
 
+function animate() {
+    ctx.clearRect (0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < explosions.length; i++) {
+        explosions[i].update();
+        explosions[i].draw();
+    }
+    requestAnimationFrame(animate);
+}
+animate();
