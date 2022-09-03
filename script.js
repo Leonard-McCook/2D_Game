322db6649811b461bc420d0ac1a39c1808be1fbd
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-09-02 23:53:18 -0400

    Create raven class for project #5

--- script.js
+++ script.js
@@ -1,65 +1,22 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 700;
-const explosions = [];
-let canvasPosition = canvas.getBoundingClientRect();
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-class Explosion {
-    constructor(x, y) {
-        this.spriteWidth  = 200;
-        this.spriteHeight = 179;
-        this.width = this.spriteWidth * 0.7;
-        this.height = this.spriteHeight * 0.7;
-        this.x = x;
-        this.y = y; 
-        this.image = new Image();
-        this.image.src = './images/boom.png';
-        this.frame = 0;
-        this.timer = 0;
-        this.angle = Math.random () * 6.2;
-        this.sound = new Audio();
-        this.sound.src = './sounds/boom.wav';
+let ravens = [];
+class Raven {
+    constructor() {
+        this.width = 100;
+        this.height = 50;
+        this.x = canvas.width;
+        this.y = Math.random() * (canvas.height - this.height);
+        this.directionX = Math.random() * 5 + 3;
+        this.directionY = Math.random() * 5 - 2.5;
     }
     update() {
-        if (this.frame === 0) this.sound.play();
-        this.timer++;
-        if (this.timer % 10 === 0) {
-            this.frame++;
-        };
+        this.x -= this.directionX;
     }
     draw() {
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.angle);
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width/2, 0 - this.height/2, this.width, this.height);
-        ctx.restore();
-    }
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }   
 }
-
-window.addEventListener('click', function(e) {
-    createAnimation(e);
-});
-// window.addEventListener('mousemove', function(e) {
-//     createAnimation(e);
-// });
-
-function createAnimation(e) {
-    let positionX = e.x - canvasPosition.left;
-    let positionY = e.y - canvasPosition.top;
-    explosions.push(new Explosion(positionX, positionY));
-}
-
-function animate() {
-    ctx.clearRect (0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < explosions.length; i++) {
-        explosions[i].update();
-        explosions[i].draw();
-        if (explosions[i].frame > 5) {
-            explosions.splice(i, 1);
-            i--;  
-        }
-    }
-    requestAnimationFrame(animate);
-}
-animate();
