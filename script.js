b5597431b47b4352d33b2620bbab0ee33ad42e01
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-09-12 22:46:51 -0400

    Implement sort method

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
             this.width = width;
             this.height = height;
             this.enemies = [];
-            this.enemyInterval = 1000;
+            this.enemyInterval = 100;
             this.enemyTimer = 0;
         }
         update(deltaTime) {
@@ -22,41 +22,43 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                 this.enemyTimer += deltaTime;
             }
-            
-            this.enemies.forEach(object => object.update());
+            this.enemies.forEach(object => object.update(deltaTime));
         }
         draw() {
             this.enemies.forEach(object => object.draw(this.ctx));
         }
         #addNewEnemy() {
             this.enemies.push(new Worm(this));
+            this.enemies.sort(function(a,b){
+                return a.y - b.y;
+            })
         }
     }
     class Enemy {
         constructor(game){
             this.game = game;
-            // console.log(this.game);
             this.markedForDeletion = false;
         }
-        update() {
-            this.x--;
-            //remove enemies
+        update(deltaTime) {
+            this.x -= this.vx * deltaTime;
             if (this.x < 0 - this.width) this.markedForDeletion = true;
         }
         draw(ctx) {
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
     }
     
     class Worm extends Enemy {
         constructor(game){
             super(game);
+            this.spriteWidth = 229;
+            this.spriteHeight = 171;
+            this.width = this.spriteWidth/2;
+            this.height = this.spriteHeight/2;
             this.x = this.game.width;
             this.y = Math.random() * this.game.height;
-            this.width = 200;
-            this.height = 100;
             this.image = worm;
-            console.log(this.image);
+            this.vx = Math.random() * 0.1 + 0.1;
         }
     }
     
