cfb3eb2367f51d09d6912c5d01fafa76a7382f26
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-09-23 21:13:35 -0400

    Add jump animation keyboard input

--- script.js
+++ script.js
@@ -39,22 +39,41 @@ window.addEventListener('load', function() {
             this.frameX = 0;
             this.frameY = 0;
             this.speed = 3;
+            this.vy = 0;
+            this.weight = 1;
         }
        draw(context) {
-            context.fillStyle = 'green';
+            context.fillStyle = 'white';
             context.fillRect(this.x, this.y, this.width, this.height);
-            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height,this.width, this.height, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
         }
         update(input){
-            //horizontal movement
-            this.x += this.speed;
             if (input.keys.indexOf('ArrowRight') > -1){
                 this.speed = 5;
             } else if (input.keys.indexOf('ArrowLeft') > -1){
                 this.speed = -5;
+            } else if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()){
+                this.vy -= 32;
             } else {
                 this.speed = 0;
             }
+            //horizontal movement
+            this.x += this.speed;
+            if (this.x < 0) this.x = 0;
+            else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+            // vertical movement
+            this.y += this.vy;
+            if (!this.onGround()){
+                this.vy += this.weight;
+                this.frameY = 1;
+            } else {
+                this.vy = 0;
+                this.frameY = 0;
+            }
+            if (this.y > this.gameHeight - this.height) this.y = thgis.gameHeight;
+        }
+        onGround(){
+            return this.y >= this.gameHeight - this.height;
         }
     }
     class Background {
