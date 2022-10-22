49f9fd6064f3804f864bd0131301060186f0637c
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-10-22 13:16:01 -0400

    Add timer and win/lose messages

--- UI.js
+++ UI.js
@@ -5,10 +5,34 @@ export class UI {
         this.fontFamily = 'Helvetica';
     }
     draw(context){
+        context.save();
+        context.shadowOffsetX = 2;
+        context.shadowOffsetY = 2;
+        context.shadowColor = 'white';
+        context.shadowBlur = 0;
         context.font = this.fontSize + 'px ' + this.fontFamily;
         context.textAlign = 'left';
         context.fillStyle = this.game.fontColor;
         // score
         context.fillText('Score: ' + this.game.score, 20, 50);
+        // time
+        context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
+        context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 20, 80);
+        // game over messages 
+        if (this.game.gameOver){
+            context.textAlign = 'center';
+            context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
+            if (this.game.score > 5){
+                context.fillText('Nice!', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText('You did a great job!', this.game.width * 0.5, this.game.height * 0.5 + 20);
+
+            } else {
+                context.fillText('You Lose!', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText('Better Luck Next Time', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            }
+        }
+        context.restore();
     }
 }
